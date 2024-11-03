aac617455738cdca76d20aab73de0fc3587b43de
Ashutosh45-maker <ashutoshparadkar05@gmail.com>
2024-11-03 15:46:43 +0530

    created classes

---
+++ main.py
@@ -0,0 +1,24 @@
+# class student :
+#     grade=12
+#     name="Ashutosh"
+    
+#     def details(self):
+#         print("Hello my name is",self.name,"and I am in grade",self.grade)
+
+# obj1=student()
+# obj1.details()
+
+#activity 2
+
+class banyan:
+    species="plant"
+
+    def __init__(self,age,name):
+     self.name = name
+     self.age = age
+
+life=banyan(160,"Alto")
+
+print("life is a ",life.species)
+print("Hello,my name is",life.name,"my age is",life.age)
+                             
